c9a0a7428cf4cc262caad021dc5e596268988dd6
JacksonLv <jianxing@redbear.cc>
2016-02-17 11:10:03 +0800

    push the Duo Setup CLI upper software (version OSX)

---
+++ DuoSetup-CLI-OSX/CMakeFiles/duo-setup-cli.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Guohui/DuoSetup-build/duo-setup-cli_automoc.cpp" "/Users/Guohui/DuoSetup-build/CMakeFiles/duo-setup-cli.dir/duo-setup-cli_automoc.cpp.o"
+  "/Users/Guohui/DuoSetup/main.cpp" "/Users/Guohui/DuoSetup-build/CMakeFiles/duo-setup-cli.dir/main.cpp.o"
+  "/Users/Guohui/DuoSetup/mainwindow.cpp" "/Users/Guohui/DuoSetup-build/CMakeFiles/duo-setup-cli.dir/mainwindow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/Users/Guohui/DuoSetup"
+  "/Users/Guohui/Qt5.3.2/5.3/clang_64/lib/QtWidgets.framework"
+  "/Users/Guohui/Qt5.3.2/5.3/clang_64/lib/QtWidgets.framework/Headers"
+  "/Users/Guohui/Qt5.3.2/5.3/clang_64/lib/QtGui.framework"
+  "/Users/Guohui/Qt5.3.2/5.3/clang_64/lib/QtGui.framework/Headers"
+  "/Users/Guohui/Qt5.3.2/5.3/clang_64/lib/QtCore.framework"
+  "/Users/Guohui/Qt5.3.2/5.3/clang_64/lib/QtCore.framework/Headers"
+  "/Users/Guohui/Qt5.3.2/5.3/clang_64/mkspecs/macx-clang"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/System/Library/Frameworks/OpenGL.framework/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
